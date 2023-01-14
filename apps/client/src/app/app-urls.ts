74c11bfc8c023c532bd1c202c08f94b51b38176d
Edwin Holguin <edwin.holguin@sinco.com.co>
2023-01-13 19:18:45 -0500

    Se crea constante de urls

---
+++ apps/client/src/app/app-urls.ts
@@ -0,0 +1,4 @@
+export const url = 'http://localhost:3333/api';
+export const apiUrl = {
+    obtenerUsuarios: `${url}/usuarios`,
+}
--- apps/client/src/app/usuario.service.ts
+++ apps/client/src/app/usuario.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UsuarioPersistencia } from '@monorepo.seminario/modelos';
+import { apiUrl } from './app-urls';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ export class UsuarioService {
 
   constructor(private http: HttpClient) { }
 
-  obtenerUsuarios(): Observable<any> {
-    return this.http.get('http://localhost:3333/api/usuarios');
+  obtenerUsuarios(): Observable<UsuarioPersistencia[]> {
+    return this.http.get<UsuarioPersistencia[]>(apiUrl.obtenerUsuarios);
   }
 }
