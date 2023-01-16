a09853dba562a3ba514ee7b1a2788646a3c4ecf5
Edwin Holguin <edwin.holguin@sinco.com.co>
2023-01-16 18:25:26 -0500

    Nuevo proyecto api-sql

---
+++ apps/api-sql/src/app/modules/usuarios/entities/usuario.entity.ts
@@ -0,0 +1,32 @@
+import { 
+    BaseEntity,
+    Column,
+    CreateDateColumn,
+    PrimaryGeneratedColumn
+} from "typeorm";
+
+export class Usuario extends BaseEntity {
+    @PrimaryGeneratedColumn('increment')
+    _id: number;
+
+    @Column({ type: 'varchar', length: 50 })
+    cedula: string;
+
+    @Column({ type: 'varchar', length: 150 })
+    nombres: string;
+
+    @Column({ type: 'varchar', length: 150 })
+    apellidos: string;
+
+    @Column({ type: 'bigint' })
+    telefono: number;
+
+    @Column({ type: 'int' })
+    horas: number;
+
+    @Column({ type: 'int' })
+    valorHora: number;
+
+    @CreateDateColumn({ type: 'timestamp', name: 'fecha_registro' })
+    createdAt: Date;
+}
